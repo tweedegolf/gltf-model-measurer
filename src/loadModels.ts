a481881271bc7c2acd52f6dbb1d242e1c351e691
abudaan <daniel@abumarkub.net>
2020-10-29 19:18:42 +0100

    removed doubling dimensions

--- src/loadModels.ts
+++ src/loadModels.ts
@@ -34,9 +34,9 @@ const loadModel = async (data: RowData, index: number) => {
     scene.scale.z = scale; // / 100;
 
     const bbox = new Box3().setFromObject(scene);
-    data.params.width = Math.round((bbox.max.x - bbox.min.x) * 2); // in tjt 50 pixels is 100cm
-    data.params.height = Math.round((bbox.max.y - bbox.min.y) * 2);
-    data.params.height3d = Math.round((bbox.max.z - bbox.min.z) * 2);
+    data.params.width = Math.round(bbox.max.x - bbox.min.x); // in tjt 50 pixels is 100cm
+    data.params.height = Math.round(bbox.max.y - bbox.min.y);
+    data.params.height3d = Math.round(bbox.max.z - bbox.min.z);
     // console.log(performance.now(), modelName);
 
     // if (typeof data.params.start !== "undefined") {

3c8c129c46280ba7539f76cd4aa936f0271c18ad
abudaan <daniel@abumarkub.net>
2020-10-30 20:44:27 +0100

    update script to support non-model 3D objects

--- src/loadModels.ts
+++ src/loadModels.ts
@@ -12,47 +12,53 @@ loader.setDRACOLoader(dracoLoader);
 const collectJSON = [];
 
 const loadModel = async (data: RowData, index: number) => {
-  const params3D = data.params_3d["3d-params"];
-  const modelName = params3D.model;
-  try {
-    const model = await loader.loadAsync(`models/${modelName}`);
-    const scene = model["scene"];
-    // console.log(data.scale);
-    // const { rotation = [0, 0, 0], scale = 100, translation = [0, 0, 0] } = data.params;
-    // const { rotation = [0, 0, 0], scale = 100, translation = [0, 0, 0] } = params3D;
-    const { rotation = [0, 0, 0], translation = [0, 0, 0] } = params3D;
-    const scale = 100;
-    scene.rotation.x = rotation[0];
-    scene.rotation.y = rotation[1];
-    scene.rotation.z = rotation[2];
+  if (data.params_3d && data.params_3d["3d-params"] && data.params_3d["3d-params"].model) {
+    const params3D = data.params_3d["3d-params"];
+    const modelName = params3D.model;
+    try {
+      const model = await loader.loadAsync(`models/${modelName}`);
+      const scene = model["scene"];
+      // console.log(data.scale);
+      // const { rotation = [0, 0, 0], scale = 100, translation = [0, 0, 0] } = data.params;
+      // const { rotation = [0, 0, 0], scale = 100, translation = [0, 0, 0] } = params3D;
+      const { rotation = [0, 0, 0], translation = [0, 0, 0] } = params3D;
+      const scale = 100;
+      scene.rotation.x = rotation[0];
+      scene.rotation.y = rotation[1];
+      scene.rotation.z = rotation[2];
 
-    // scene.position.x = translation[0];
-    // scene.position.y = translation[1];
-    // scene.position.z = translation[2];
-    scene.scale.x = scale; // / 100;
-    scene.scale.y = scale; // / 100;
-    scene.scale.z = scale; // / 100;
+      // scene.position.x = translation[0];
+      // scene.position.y = translation[1];
+      // scene.position.z = translation[2];
+      scene.scale.x = scale; // / 100;
+      scene.scale.y = scale; // / 100;
+      scene.scale.z = scale; // / 100;
 
-    const bbox = new Box3().setFromObject(scene);
-    data.params.width = Math.round(bbox.max.x - bbox.min.x); // in tjt 50 pixels is 100cm
-    data.params.height = Math.round(bbox.max.y - bbox.min.y);
-    data.params.height3d = Math.round(bbox.max.z - bbox.min.z);
-    // console.log(performance.now(), modelName);
+      const bbox = new Box3().setFromObject(scene);
+      data.params.width = Math.round(bbox.max.x - bbox.min.x); // in tjt 50 pixels is 100cm
+      data.params.height = Math.round(bbox.max.y - bbox.min.y);
+      data.params.height3d = Math.round(bbox.max.z - bbox.min.z);
+      // console.log(performance.now(), modelName);
 
-    // if (typeof data.params.start !== "undefined") {
-    //   data.params.start *= 2;
-    // }
+      // if (typeof data.params.start !== "undefined") {
+      //   data.params.start *= 2;
+      // }
 
-    // console.log(width, height, height3d);
+      // console.log(width, height, height3d);
+      collectJSON.push(data);
+
+      setTimeout(() => {
+        useStore3D.setState({ model: scene, modelIndex: index + 1 });
+        // useStore3D.setState({ modelIndex: index + 1 });
+        // useStore3D.setState({ model: scene });
+      }, 25);
+    } catch (e) {
+      console.error(`"${modelName}" could not be found`, e.message);
+      useStore3D.setState({ modelIndex: index + 1 });
+    }
+  } else {
+    console.log("no params_3d", data.title);
     collectJSON.push(data);
-
-    setTimeout(() => {
-      useStore3D.setState({ model: scene, modelIndex: index + 1 });
-      // useStore3D.setState({ modelIndex: index + 1 });
-      // useStore3D.setState({ model: scene });
-    }, 25);
-  } catch (e) {
-    console.error(`"${modelName}" could not be found`, e.message);
     useStore3D.setState({ modelIndex: index + 1 });
   }
 };
